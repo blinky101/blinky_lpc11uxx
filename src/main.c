c9a1a46dad1066b570272ac06d9d30d299ed3969
Ingmar Jager <ingmarjager@gmail.com>
2018-08-29 12:07:52 +0200

    Add uart peripheral example.
    This can be used for printf debugging using the blackmagic probe or any
    other serial -> usb converter.

--- src/main.c
+++ src/main.c
@@ -8,10 +8,69 @@
 #include <mcu_timing/delay.h>
 
 
+/* Transmit and receive ring buffers */
+STATIC RINGBUFF_T txring, rxring;
+
+/* Transmit and receive ring buffer sizes */
+#define UART_SRB_SIZE 128	/* Send */
+#define UART_RRB_SIZE 32	/* Receive */
+
+/* Transmit and receive buffers */
+static uint8_t rxbuff[UART_RRB_SIZE], txbuff[UART_SRB_SIZE];
+
+const char inst1[] = "LPC11xx UART example using ring buffers\r\n";
+const char inst2[] = "Press a key to echo it back or ESC to quit\r\n";
+const char hb[] = "boom boom\r\n";
+
+static void Init_UART_PinMux(void)
+{
+	Chip_IOCON_PinMuxSet(LPC_IOCON, 0, 18, IOCON_FUNC1 | IOCON_MODE_INACT);	/* PIO0_18 used for RXD */
+	Chip_IOCON_PinMuxSet(LPC_IOCON, 0, 19, IOCON_FUNC1 | IOCON_MODE_INACT);	/* PIO0_19 used for TXD */
+}
+
+/**
+ * @brief	UART interrupt handler using ring buffers
+ * @return	Nothing
+ */
+void UART_IRQHandler(void)
+{
+	/* Want to handle any errors? Do it here. */
+
+	/* Use default ring buffer handler. Override this with your own
+	   code if you need more capability. */
+	Chip_UART_IRQRBHandler(LPC_USART, &rxring, &txring);
+}
+
 // TODO RM or interrupt -based
 void SysTick_Handler(void)
 {
 
+}
+
+static void Uart_Init(void)
+{
+	Init_UART_PinMux();
+
+	/* Setup UART for 115.2K8N1 */
+	Chip_UART_Init(LPC_USART);
+	Chip_UART_SetBaud(LPC_USART, 115200);
+	Chip_UART_ConfigData(LPC_USART, (UART_LCR_WLEN8 | UART_LCR_SBS_1BIT));
+	Chip_UART_SetupFIFOS(LPC_USART, (UART_FCR_FIFO_EN | UART_FCR_TRG_LEV2));
+	Chip_UART_TXEnable(LPC_USART);
+
+	/* Before using the ring buffers, initialize them using the ring
+	   buffer init function */
+	RingBuffer_Init(&rxring, rxbuff, 1, UART_RRB_SIZE);
+	RingBuffer_Init(&txring, txbuff, 1, UART_SRB_SIZE);
+
+	/* Enable receive data and line status interrupt */
+	Chip_UART_IntEnable(LPC_USART, (UART_IER_RBRINT | UART_IER_RLSINT));
+
+	/* preemption = 1, sub-priority = 1 */
+	NVIC_SetPriority(UART0_IRQn, 1);
+	NVIC_EnableIRQ(UART0_IRQn);
+
+	
 }
 
 // TODO it would be nice to add clock_set_frequency() support via lpc_tools
@@ -89,12 +148,17 @@ int main(void)
 
     // get the GPIO with the led (see board.c)
     const GPIO *led = board_get_GPIO(GPIO_ID_LED);
+	Uart_Init();
 
+	/* Send initial messages */
+	Chip_UART_SendRB(LPC_USART, &txring, inst1, sizeof(inst1) - 1);
+	Chip_UART_SendRB(LPC_USART, &txring, inst2, sizeof(inst2) - 1);
 	while(true)
 	{
         // LED toggle
         GPIO_HAL_toggle(led);
-        delay_us(100*1000);
+        delay_us(1000*1000);
+		Chip_UART_SendRB(LPC_USART, &txring, hb, sizeof(hb) - 1);
 	}
 	return 0;
 }
